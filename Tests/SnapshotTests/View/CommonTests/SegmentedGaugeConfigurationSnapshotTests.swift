110669e13728bbe93ebb43101b0dc24bf105aada
Robin Lemaire <robin.lemaire@adevinta.com>
2025-11-12 10:15:04 +0100

    [Test] Add Snapshots

---
+++ Tests/SnapshotTests/View/CommonTests/SegmentedGaugeConfigurationSnapshotTests.swift
@@ -0,0 +1,108 @@
+//
+//  SegmentedGaugeConfigurationSnapshotTests.swift
+//  SparkComponentSegmentedGaugeSnapshotTests
+//
+//  Created by robin.lemaire on 29/10/2025.
+//  Copyright © 2025 Leboncoin. All rights reserved.
+//
+
+import UIKit
+@testable import SparkComponentSegmentedGauge
+@_spi(SI_SPI) import SparkComponentSegmentedGaugeTesting
+@_spi(SI_SPI) import SparkCommon
+@_spi(SI_SPI) import SparkCommonSnapshotTesting
+
+struct SegmentedGaugeConfigurationSnapshotTests {
+
+    // MARK: - Properties
+
+    let scenario: SegmentedGaugeScenarioSnapshotTests
+
+    var alignment: SegmentedGaugeAlignment = .default
+    var isMarker: Bool = true
+    var segments: SegmentedGaugeSegments = .default
+    var size: SegmentedGaugeSize = .default
+    var type: SegmentedGaugeTestingType = .default
+    var title: SegmentedGaugeTitle = .default
+    var description: SegmentedGaugeDescription = .default
+
+    var modes: [ComponentSnapshotTestMode] = ComponentSnapshotTestConstants.Modes.default
+    var sizes: [UIContentSizeCategory] = ComponentSnapshotTestConstants.Sizes.default
+
+    // MARK: - Name
+
+    var name: String {
+        guard self.documentationName == nil else {
+            return ""
+        }
+
+        return [
+            "\(self.alignment)" + "Alignment",
+            self.isMarker ? "isMarker" : nil,
+            "\(self.segments)" + "Segments",
+            "\(self.size)" + "Size",
+            "\(self.type)" + "Type",
+            "\(self.title)" + "Title",
+            "\(self.description)" + "Description"
+        ]
+            .compactMap { $0 }
+            .joined(separator: "-")
+    }
+
+    var testName: String {
+        return if let documentationName {
+            "segmented_gauge_" + documentationName
+        } else {
+            self.scenario.rawValue
+        }
+    }
+
+    var documentationName: String?
+}
+
+// MARK: - Enum
+
+enum SegmentedGaugeTitle: String, CaseIterable {
+    case withoutText
+    case text
+    case other
+
+    static var `default` = Self.text
+
+    var text: String? {
+        switch self {
+        case .withoutText: nil
+        case .text: "My component"
+        case .other: nil
+        }
+    }
+
+    var documentationName: String {
+        switch self {
+        case .withoutText: ""
+        case .text: "title"
+        case .other: "label"
+        }
+    }
+}
+
+enum SegmentedGaugeDescription: String, CaseIterable {
+    case text
+    case other
+
+    static var `default` = Self.text
+
+    var text: String? {
+        switch self {
+        case .text: "The description"
+        case .other: nil
+        }
+    }
+
+    var documentationName: String {
+        switch self {
+        case .text: "description"
+        case .other: "label"
+        }
+    }
+}
---
+++ Tests/SnapshotTests/View/SparkUISegmentedGaugeSnapshotTests.swift
@@ -0,0 +1,101 @@
+//
+//  SparkUISegmentedGaugeSnapshotTests.swift
+//  SparkComponentSegmentedGaugeSnapshotTests
+//
+//  Created by robin.lemaire on 29/10/2025.
+//  Copyright © 2025 Leboncoin. All rights reserved.
+//
+
+import XCTest
+import SwiftUI
+import SnapshotTesting
+@testable import SparkComponentSegmentedGauge
+@_spi(SI_SPI) import SparkComponentSegmentedGaugeTesting
+@_spi(SI_SPI) import SparkCommon
+@_spi(SI_SPI) import SparkCommonSnapshotTesting
+@_spi(SI_SPI) import SparkCommonTesting
+import SparkTheming
+import SparkTheme
+
+final class SparkUISegmentedGaugeSnapshotTests: UIKitComponentSnapshotTestCase {
+
+    // MARK: - Properties
+
+    private let theme: any Theme = SparkTheme.shared
+
+    // MARK: - Tests
+
+    func test() {
+        let scenarios = SegmentedGaugeScenarioSnapshotTests.allCases.filter {
+            $0 != .documentation
+        }
+
+        for scenario in scenarios {
+            let configurations = scenario.configuration()
+            for configuration in configurations {
+                let segmentedGauge = SparkUISegmentedGauge(theme: self.theme)
+                segmentedGauge.alignment = configuration.alignment
+                segmentedGauge.isMarker = configuration.isMarker
+                segmentedGauge.segments = configuration.segments
+                segmentedGauge.size = configuration.size
+                segmentedGauge.type = configuration.type.toType(self.theme)
+                segmentedGauge.title(configuration.title)
+                segmentedGauge.description(configuration.description)
+
+                let backgroundView = UIView()
+                backgroundView.backgroundColor = .systemBackground
+                backgroundView.translatesAutoresizingMaskIntoConstraints = false
+                backgroundView.addSubview(segmentedGauge)
+
+                NSLayoutConstraint.stickEdges(
+                    from: segmentedGauge,
+                    to: backgroundView,
+                    insets: .init(all: 20)
+                )
+
+                self.assertSnapshot(
+                    matching: backgroundView,
+                    named: configuration.name,
+                    modes: configuration.modes,
+                    sizes: configuration.sizes,
+                    testName: configuration.testName
+                )
+            }
+        }
+    }
+}
+
+// MARK: - Extension
+
+private extension SparkUISegmentedGauge {
+
+    func title(_ title: SegmentedGaugeTitle) {
+        if let text = title.text {
+            self.title = text
+        } else if title == .other {
+            self.attributedTitle = .init(
+                string: "Welcome\non Spark's segmented gauge",
+                attributes: [
+                    .foregroundColor: UIColor.red,
+                    .font: UIFont.italicSystemFont(ofSize: 20),
+                    .underlineStyle: NSUnderlineStyle.single.rawValue
+                ]
+            )
+        }
+    }
+
+    func description(_ description: SegmentedGaugeDescription) {
+        if let text = description.text {
+            self.descriptionString = text
+        } else if description == .other {
+            self.attributedDescription = .init(
+                string: "This is\na description",
+                attributes: [
+                    .foregroundColor: UIColor.green,
+                    .font: UIFont.italicSystemFont(ofSize: 14),
+                    .underlineStyle: NSUnderlineStyle.single.rawValue
+                ]
+            )
+        }
+    }
+}
